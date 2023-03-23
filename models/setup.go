991cfcc69a7fc9d1ae042d583b05940fb962ee56
アリフリュウ <49254668+zoelabbb@users.noreply.github.com>
2023-03-23 17:13:19 +0700

    New Config DB
    Requirements :
    - DB Using PlanetScale

--- models/setup.go
+++ models/setup.go
@@ -8,12 +8,14 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open(mysql.Open("root:@tcp(localhost:3306)/go_restpi_gin"))
+	// keys := "DSN"
+	// dsn := "29iju1fem3r93rb4k8ci:pscale_pw_IncoiqiF88s9HNwnyNYkfGxIYbT5Rk3SDDSA2yGhea1@tcp(ap-southeast.connect.psdb.cloud)/go_restpi_gin?tls=true"
+	database, err := gorm.Open(mysql.Open("yn6xhx6hndet7wkh36js:pscale_pw_DhYO0DMBR700TMhsfNINbEhUfN2SRdeikETNaOP0ytw@tcp(ap-southeast.connect.psdb.cloud)/go_restpi_gin?tls=true"))
 	if err != nil {
 		panic(err)
 	}
 
-	database.AutoMigrate(&Product{})
+	database.AutoMigrate(&Pasien{})
 
 	DB = database
 }
